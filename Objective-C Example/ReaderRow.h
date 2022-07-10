99178f3445998f8bc923c246c2fcb8c1ee122d54
Andrew Kingdom <1809762+akingdom@users.noreply.github.com>
2022-07-11 03:10:52 +1000

    Create ReaderRow.h

---
+++ Objective-C Example/ReaderRow.h
@@ -0,0 +1,24 @@
+//
+//  ReaderRow.h
+//
+//  Copyright © 2018 BNotro Software Development Pty Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ReaderRow : NSObject
+
+@property (strong, nonatomic) NSMutableDictionary * values;  // of NSStrings
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+/*
+ReaderRow.m is an empty class...
+#import "ReaderRow.h"
+@implementation ReaderRow
+@end
+*/
